69da03edb915f1e9027f83730f08e27d785d550b
JonahBenares <jonahbenares.cenpri@gmail.com>
2020-07-07 14:30:04 +0800

    fix checkpritem

--- application/controllers/Request.php
+++ application/controllers/Request.php
@@ -420,17 +420,18 @@ class Request extends CI_Controller {
 
           $recqty = $this->super_model->custom_query_single("received_qty","SELECT ri.received_qty FROM receive_items ri INNER JOIN receive_details rd ON ri.rd_id = rd.rd_id INNER JOIN receive_head rh ON rd.receive_id = rh.receive_id WHERE rh.saved = '1' AND rd.pr_no = '$pr' AND ri.item_id = '$item'");
 
+           $issue_qty = $this->super_model->custom_query_single("issueqty","SELECT SUM(quantity) AS issueqty FROM issuance_head ih INNER JOIN issuance_details id ON ih.issuance_id = id.issuance_id WHERE pr_no= '$pr' AND item_id='$item'");
 
-        $qty=array(0);
+      /*  $qty=array(0);
         foreach($this->super_model->select_row_where("request_head", "pr_no", $pr) AS $req){
             //echo $req->request_id . " - " . $item . "<br>";
                 foreach($this->super_model->select_custom_where("request_items", "request_id ='$req->request_id' AND item_id = '$item'") AS $ri){
                // echo $ri->rq_id;
                  $qty[]=$this->super_model->select_column_where("issuance_details", "quantity", "rq_id", $ri->rq_id);
                 }
-        }
+        }*/
 
-        $issue_qty = array_sum($qty);
+        //$issue_qty = array_sum($qty);
         $bal=($recqty-$issue_qty);
         echo $bal;
     }
