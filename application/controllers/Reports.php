e4a75c0e104e3664082ebe125e74266ebb94be63
Jonah Faye Benares <jonahbenares.cenpri@gmail.com>
2021-02-05 22:56:44 +0800

    fix sort stockcard

--- application/controllers/Reports.php
+++ application/controllers/Reports.php
@@ -1698,18 +1698,20 @@ class Reports extends CI_Controller {
                 'method'=>'Beginning Balance',
                 'quantity'=>$begbal->quantity,
                 'series'=>'1',
-                'date'=>''
+                'date'=>'',
+                'create_date'=>''
             );
 
             $data['balance'][] = array(
                 'series'=>'1',
                 'method'=>'Beginning Balance',
                 'quantity'=>$begbal->quantity,
-                'date'=>''
+                'date'=>'',
+                'create_date'=>''
             );
         }
 
-        foreach($this->super_model->custom_query("SELECT rh.receive_id,rh.receive_date, ri.supplier_id, ri.brand_id, ri.catalog_no, ri.received_qty, ri.item_cost, ri.rd_id FROM receive_head rh INNER JOIN receive_items ri ON rh.receive_id = ri.receive_id WHERE $query AND saved = '1'") AS $receive){
+        foreach($this->super_model->custom_query("SELECT rh.receive_id,rh.receive_date, ri.supplier_id, ri.brand_id, ri.catalog_no, ri.received_qty, ri.item_cost, ri.rd_id,  rh.create_date FROM receive_head rh INNER JOIN receive_items ri ON rh.receive_id = ri.receive_id WHERE $query AND saved = '1'") AS $receive){
             $pr_no = $this->super_model->select_column_where("receive_details", "pr_no", "rd_id", $receive->rd_id);
             $supplier = $this->super_model->select_column_where("supplier", "supplier_name", "supplier_id", $receive->supplier_id);
              $brand = $this->super_model->select_column_where("brand", "brand_name", "brand_id", $receive->brand_id);
@@ -1722,19 +1724,21 @@ class Reports extends CI_Controller {
                 'method'=>'Receive',
                 'series'=>'2',
                 'quantity'=>$receive->received_qty,
-                'date'=>$receive->receive_date
+                'date'=>$receive->receive_date,
+                'create_date'=>$receive->create_date
             );
              $data['balance'][] = array(
                 'series'=>'2',
                 'method'=>'Receive',
                 'quantity'=>$receive->received_qty,
-                'date'=>$receive->receive_date
+                'date'=>$receive->receive_date,
+                'create_date'=>$receive->create_date
             );
         }
 
         //echo "****SELECT ih.issue_date, id.rq_id, id.supplier_id, id.brand_id, id.catalog_no, id.quantity FROM issuance_head ih INNER JOIN issuance_details id ON ih.issuance_id = id.issuance_id WHERE $query";
 
-        foreach($this->super_model->custom_query("SELECT ih.issue_date, ih.pr_no, id.rq_id, id.supplier_id, id.brand_id, id.catalog_no, id.quantity FROM issuance_head ih INNER JOIN issuance_details id ON ih.issuance_id = id.issuance_id WHERE $query AND saved = '1'") AS $issue){
+        foreach($this->super_model->custom_query("SELECT ih.issue_date, ih.pr_no, id.rq_id, id.supplier_id, id.brand_id, id.catalog_no, id.quantity, ih.create_date  FROM issuance_head ih INNER JOIN issuance_details id ON ih.issuance_id = id.issuance_id WHERE $query AND saved = '1'") AS $issue){
             $cost = $this->super_model->select_column_where("request_items", "unit_cost", "rq_id", $issue->rq_id);
             $supplier = $this->super_model->select_column_where("supplier", "supplier_name", "supplier_id", $issue->supplier_id);
              $brand = $this->super_model->select_column_where("brand", "brand_name", "brand_id", $issue->brand_id);
@@ -1747,19 +1751,21 @@ class Reports extends CI_Controller {
                 'method'=>'Issuance',
                 'series'=>'3',
                 'quantity'=>$issue->quantity,
-                'date'=>$issue->issue_date
+                'date'=>$issue->issue_date,
+                'create_date'=>$issue->create_date
             );
 
             $data['balance'][] = array(
                 'series'=>'3',
                 'method'=>'Issuance',
                 'quantity'=>$issue->quantity,
-                'date'=>$issue->issue_date
+                'date'=>$issue->issue_date,
+                'create_date'=>$issue->create_date
             );
 
         }
 
-         foreach($this->super_model->custom_query("SELECT rh.restock_date, rh.from_pr, ri.supplier_id, ri.brand_id, ri.catalog_no, ri.quantity, ri.item_cost FROM restock_head rh INNER JOIN restock_details ri ON rh.rhead_id = ri.rhead_id WHERE $query AND saved = '1' AND excess='0'") AS $restock){
+         foreach($this->super_model->custom_query("SELECT rh.restock_date, rh.create_date, rh.from_pr, ri.supplier_id, ri.brand_id, ri.catalog_no, ri.quantity, ri.item_cost FROM restock_head rh INNER JOIN restock_details ri ON rh.rhead_id = ri.rhead_id WHERE $query AND saved = '1' AND excess='0'") AS $restock){
             
             $supplier = $this->super_model->select_column_where("supplier", "supplier_name", "supplier_id", $restock->supplier_id);
             $brand = $this->super_model->select_column_where("brand", "brand_name", "brand_id", $restock->brand_id);
@@ -1772,13 +1778,15 @@ class Reports extends CI_Controller {
                 'method'=>'Restock',
                 'series'=>'4',
                 'quantity'=>$restock->quantity,
-                'date'=>$restock->restock_date
+                'date'=>$restock->restock_date,
+                'create_date'=>$restock->restock_date
             );
             $data['balance'][] = array(
                 'series'=>'4',
                 'method'=>'Restock',
                 'quantity'=>$restock->quantity,
-                'date'=>$restock->restock_date
+                'date'=>$restock->restock_date,
+                'create_date'=>$restock->restock_date
             );
 
         }
